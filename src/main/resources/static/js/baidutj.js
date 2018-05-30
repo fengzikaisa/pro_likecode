4b9db400996a8771911eaad52eca545aed4afc00
wangkai <wangkai@senyint.com>
2018-05-30 15:02:45 +0800

    添加.club 百度统计代码

--- src/main/resources/static/js/baidutj.js
+++ src/main/resources/static/js/baidutj.js
@@ -1,7 +1,16 @@
+//.top
+// var _hmt = _hmt || [];
+// (function() {
+//     var hm = document.createElement("script");
+//     hm.src = "https://hm.baidu.com/hm.js?9cb60526dc4a2798c3a72b8b3a4d0f0f";
+//     var s = document.getElementsByTagName("script")[0];
+//     s.parentNode.insertBefore(hm, s);
+// })();
+//.club
 var _hmt = _hmt || [];
 (function() {
     var hm = document.createElement("script");
-    hm.src = "https://hm.baidu.com/hm.js?9cb60526dc4a2798c3a72b8b3a4d0f0f";
+    hm.src = "https://hm.baidu.com/hm.js?e55262211f1df7b6717941c0be651895";
     var s = document.getElementsByTagName("script")[0];
     s.parentNode.insertBefore(hm, s);
 })();
